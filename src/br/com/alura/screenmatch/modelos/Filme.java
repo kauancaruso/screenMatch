e71b686e197cf2a0dabb61555acf94b4ba3893ce
kauancaruso <k4u4n.br1to@gmail.com>
2024-04-04 15:08:49 -0300

    first commit

---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Filme extends Titulo implements Classificavel {
+    private String direto;
+
+    public String getDireto() {
+        return direto;
+    }
+
+    public void setDireto(String direto) {
+        this.direto = direto;
+    }
+
+    @Override
+    public int getClassificacao() {
+        return (int)pegaMedia() / 2;
+    }
+}
